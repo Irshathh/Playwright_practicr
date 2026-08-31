4857e7c304f609b0d45594903ee0596c5c08655c
Irshathh <163999846+Irshathh@users.noreply.github.com>
2026-09-01 01:58:31 +0530

    Change test from only to regular for element screenshot

--- tests/screenshot.spec.js
+++ tests/screenshot.spec.js
@@ -21,7 +21,7 @@ test('Full page screenshot',async({page})=>{
     
 })
 
-test.only('Element screenshot', async({page})=>{
+test('Element screenshot', async({page})=>{
     await page.goto('https://www.amazon.in/HOKIPO-Portable-Dustbin-Countertop-Desktop/dp/B0G5FDFV35/?_encoding=UTF8&pd_rd_w=Gc2qa&content-id=amzn1.sym.263478fa-03d8-4a98-887c-61621826f852&pf_rd_p=263478fa-03d8-4a98-887c-61621826f852&pf_rd_r=MJBBAWA6BFP23VSA6VNF&pd_rd_wg=Q1Yrv&pd_rd_r=280c7f29-4b9f-4a5e-a1b6-31bccca60807&ref_=pd_hp_d_atf_dealz_cs&th=1')
 
 
